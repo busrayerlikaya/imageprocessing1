de44fbf59816a96590f219e307b1ea3de53075c7
busrayerlikaya <bsrayerlikaya@gmail.com>
2018-10-09 02:29:29 +0300

    image processing

---
+++ Griye Çevirme Yöntemleri/Griye Çevirme Yöntemleri/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Griye_Çevirme_Yöntemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.temizleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.temizleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(834, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem,
+            this.kaydetToolStripMenuItem,
+            this.kapatToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // ekleToolStripMenuItem
+            // 
+            this.ekleToolStripMenuItem.Name = "ekleToolStripMenuItem";
+            this.ekleToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.ekleToolStripMenuItem.Text = "Aç";
+            this.ekleToolStripMenuItem.Click += new System.EventHandler(this.ekleToolStripMenuItem_Click);
+            // 
+            // kaydetToolStripMenuItem
+            // 
+            this.kaydetToolStripMenuItem.Name = "kaydetToolStripMenuItem";
+            this.kaydetToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.kaydetToolStripMenuItem.Text = "Kaydet";
+            this.kaydetToolStripMenuItem.Click += new System.EventHandler(this.kaydetToolStripMenuItem_Click);
+            // 
+            // kapatToolStripMenuItem
+            // 
+            this.kapatToolStripMenuItem.Name = "kapatToolStripMenuItem";
+            this.kapatToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.kapatToolStripMenuItem.Text = "Kapat";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 433);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ortalama";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(418, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Luma";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // temizleToolStripMenuItem
+            // 
+            this.temizleToolStripMenuItem.Name = "temizleToolStripMenuItem";
+            this.temizleToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.temizleToolStripMenuItem.Text = "Temizle";
+            this.temizleToolStripMenuItem.Click += new System.EventHandler(this.temizleToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 433);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "BT-709";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(255, 433);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Açıklık";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(336, 433);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Extract";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(834, 472);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Resmi Griye Çevirme Yöntemleri";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kapatToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem kaydetToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem temizleToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Griye Çevirme Yöntemleri/Griye Çevirme Yöntemleri/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Griye_Çevirme_Yöntemleri
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        Bitmap Resim, cevir, kayit;
+        int max;
+        int min;
+       
+
+
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            DialogResult sonuc = openFileDialog1.ShowDialog();
+            
+
+            if (sonuc == DialogResult.OK)
+            {
+
+                Resim = new Bitmap(openFileDialog1.FileName);
+
+                pictureBox1.Image = Resim;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int a = Resim.Width;
+            int b = Resim.Height;
+
+             cevir = new Bitmap(a, b);
+
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    Color renkli = Resim.GetPixel(x, y);
+                    int gri = (int)((renkli.R* .3) + (renkli.G*.59) + (renkli.B*.11));
+                    Color grili = Color.FromArgb(gri, gri, gri);
+                    cevir.SetPixel(x, y, grili);
+
+                }
+            }
+
+            pictureBox2.Image = cevir;
+        }
+
+        private void temizleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Resim.Width;
+            int b = Resim.Height;
+
+            cevir = new Bitmap(a, b);
+
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    Color renkli = Resim.GetPixel(x, y);
+                    int gri = (int)((renkli.R * 0.2125) + (renkli.G * 0.7154) + (renkli.B * 0.072));
+                    Color grili = Color.FromArgb(gri, gri, gri);
+                    cevir.SetPixel(x, y, grili);
+
+                }
+            }
+
+            pictureBox2.Image = cevir;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = Resim.Width;
+            int b = Resim.Height;
+            
+
+            cevir = new Bitmap(a, b);
+
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    Color renkli = Resim.GetPixel(x, y);
+
+                    if (renkli.R > renkli.G && renkli.R > renkli.B) { max = renkli.R; }
+                    if (renkli.G > renkli.R && renkli.G > renkli.B) { max = renkli.G; }
+                    if (renkli.B > renkli.G && renkli.B > renkli.R) { max = renkli.B; }
+
+                    if (renkli.R < renkli.G && renkli.R < renkli.B) { min = renkli.R; }
+                    if (renkli.G < renkli.R && renkli.G < renkli.B) { min = renkli.G; }
+                    if (renkli.B < renkli.G && renkli.B < renkli.R) { min = renkli.B; }
+
+
+
+
+
+                    int gri =(int) (max+min) / 2;
+                    Color grili = Color.FromArgb(gri, gri, gri);
+                    cevir.SetPixel(x, y, grili);
+
+                }
+            }
+
+            pictureBox2.Image = cevir;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int a = Resim.Width;
+            int b = Resim.Height;
+
+            cevir = new Bitmap(a, b);
+
+            for (int x = 0; x < a; x++)
+            {
+                for (int y = 0; y < b; y++)
+                {
+                    
+                    Color renkli = Resim.GetPixel(x, y);
+                    int c = renkli.A;
+                    int r = renkli.R;
+                    int g = renkli.G;
+                    int d = renkli.B;
+                    
+                    
+                     cevir.SetPixel(x, y, Color.FromArgb(Resim.GetPixel(x,y).A,Resim.GetPixel(x,y).R,0,0)); 
+                    
+
+                }
+            }
+
+            pictureBox2.Image = cevir;
+        }
+
+        private void kaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "PNG|*.png";
+            DialogResult sonuc = saveFileDialog1.ShowDialog();
+            ImageFormat format = ImageFormat.Png;
+            if (sonuc == DialogResult.OK)
+            {
+                cevir.Save(saveFileDialog1.FileName, format);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Resim.Width;
+            int b = Resim.Height;
+
+            cevir = new Bitmap(a, b);
+            
+            for( int x=0; x < a; x++)
+            {
+                for(int y=0;y<b;y++)
+                {
+                    Color renkli = Resim.GetPixel(x, y);
+                    int gri = (renkli.R + renkli.G + renkli.B) / 3;
+                    Color grili = Color.FromArgb(gri, gri, gri);
+                    cevir.SetPixel(x, y, grili);
+
+                }
+            }
+
+            pictureBox2.Image = cevir;
+
+        }
+    }
+}
